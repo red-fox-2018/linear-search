27ac9bb64d32bfdf6ba5ed22a70cfb427e49bcc7
bramaprasetyopermana <bramprasetyop@gmail.com>
2018-04-03 15:19:07 +0700

    linear-search-brama-prasetyo

--- linear.js
+++ linear.js
@@ -1,9 +1,14 @@
 'use strict'
 
 // Release 0
-console.log("Linear Search")
+// console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (target===values[i]) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -14,17 +19,18 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
-console.log("Global Linear Search")
+// console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var result=[]
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      result.push(i)
+    }
+  }
+  return result;
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
-
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
