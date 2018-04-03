0126cae1bc482df99cecedc0c69ae4c0177227f8
alprak <you@example.com>
2018-04-03 15:38:51 +0700

    done

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,15 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(var i=0;i<values.length;i++) {
+  	//console.log(values.length);
+  	if(target===values[i]) {
+  		return i
+    }
+  
+  } 
+  return -1
+
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -13,10 +22,22 @@ console.log(linearSearch(18, random_numbers));
 console.log(linearSearch(9, random_numbers));
 // -1
 
+
+
+
+
+
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var output=[];
+  for(var i=0;i<values.length;i++) {
+  	if(values[i]===target) {
+      output.push(i);
+  	}
+  }
+  return output;
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +45,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
