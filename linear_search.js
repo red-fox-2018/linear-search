7d38db77080e953bd0f7b168b2a4ba3839715030
restuu <restu_u@outlook.com>
2018-04-03 14:18:12 +0700

    first commit

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,12 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -11,12 +16,18 @@ let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 console.log(linearSearch(18, random_numbers));
 // 2
 console.log(linearSearch(9, random_numbers));
-// -1
+-1
 
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  var indArr = [];
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      indArr.push(i);
+    }
+  }
+  return indArr;
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +35,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
