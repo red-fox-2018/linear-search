4e3667879c68aa8cbaeb8ec2481ded90b59fa194
violerine <viosefferine@gmail.com>
2018-04-03 18:28:24 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -1,30 +1,19 @@
-'use strict'
-
-// Release 0
-console.log("Linear Search")
-let linearSearch = (target, values) => {
-  //write your code here
+let random_numbers=[6,29,18,2,72,19,18,10,37]
+function linearSearch(num,array){
+    var answer=array.indexOf(num)
+    return answer
 }
+console.log(linearSearch(18,random_numbers))
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
 
-console.log(linearSearch(18, random_numbers));
-// 2
-console.log(linearSearch(9, random_numbers));
-// -1
-
-// Release 1
-console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
-  //write your code here
+let banana_arr="banana".split("")
+function globalLinearSearch(input,array){
+    var answer=[]
+    for(var i=0; i<array.length; i++){
+        if(input===array[i]){
+            answer.push(i)
+        }
+    }
+    return answer
 }
-
-let banana_arr = "banana".split("");
-// ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
-// [ 1, 3, 5 ]
-
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+console.log(globalLinearSearch("a",banana_arr))
