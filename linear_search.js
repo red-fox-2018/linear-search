b61101655bc042e162c357da9acb01c38fbfac99
annisaayu <annisaayu569@gmail.com>
2018-04-04 05:55:12 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(let i = 0; i < values.length; i++){
+    if(values[i] === target){
+      return i
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +23,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let arrResult = []
+  for(let i = 0; i < values.length; i++){
+    if(values[i] === target){
+      arrResult.push(i)
+    }
+  }
+  return arrResult
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +37,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
