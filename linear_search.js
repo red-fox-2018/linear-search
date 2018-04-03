1d2ed463c984c1ae544b1891d5467ae88620b4e8
Iswanul Umam <umam@sepulsa.com>
2018-04-03 13:43:47 +0700

    finish linier search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,14 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  let result = -1;
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      result = i;
+      break;
+    }
+  }
+  return result;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +23,14 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  // write your code here
+  let ans = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      ans.push(i);
+    }
+  }
+  return ans;
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +38,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
